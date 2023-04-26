c46ef22d8f4cba45f587c5320659055009ebc2ca
Xu Shihao <xushihao@localhost>
2023-04-26 16:29:46 +0900

    section19 generics

---
+++ Go/section19/160genericsVector.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+type Vector[T any] []T
+
+type IntVector Vector[int]
+
+func main() {
+	var v Vector[int] = Vector[int]{10, 20, 30}
+	fmt.Println(v)
+
+	v2 := Vector[float64]{1.3, 3.4, 4.5}
+	fmt.Println(v2)
+
+	v3 := IntVector{1, 2, 3}
+	fmt.Println(v3)
+}
